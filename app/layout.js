7989c6a3078ded0a0681d5cc0e6e1f69bcb3dc0d
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-05-13 21:43:13 +0600

    about section done

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="hydrated">
+    <html lang="en" className="hydrated scroll-smooth">
       <body
         className={`${outfit.className
         } ${ovo.className} antialiased leading-8 overflow-x-hidden`}
